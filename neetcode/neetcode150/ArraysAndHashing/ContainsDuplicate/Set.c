a6d7703ad4d988eff34488299d89841a87afbbc4
Christopher Francisco <cjfg00gl3@gmail.com>
2025-01-14 20:58:15 -0600

    UPDATE Set.c by CREATE create function to create a new Set

--- neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/Set.c
+++ neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/Set.c
@@ -1,11 +1,19 @@
 #include "stdio.h"
 
-typedef struct { // our custom set for the problem which only holds digits
-    Set* digits;
+typedef struct Set // our custom set will hold digit values
+{ 
+    struct Set* digits;
 } Set;
+
+Set* create()
+{
+    Set* newSet = (Set*)malloc(sizeof(Set)); // we've allocated memory for the new set
+    return newSet;
+}
 
 int main(void)
 {
-
-
+    Set* digits = create(); // we've created our digits set
+    printf("HELLO WORLD!\n");
+    return 0;
 }
