a80c1212fb67177d919ef3eb28f3d885fac843cb
Christopher Francisco <cjfg00gl3@gmail.com>
2025-01-14 22:24:27 -0600

    UPATE Set.c by CREATE add function to add / record values in the set

--- neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/Set.c
+++ neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/Set.c
@@ -20,6 +20,36 @@ Set* create() // Returns a pointer to a new Set
     return newSet;
 }
 
+void add(Set* set, int value)
+{
+    if (set == NULL) // a null set was provided
+    {
+        fprintf(stderr, "TRYING TO ADD A VALUE TO A NULL SET\n");
+        return;
+    }
+    int temp = value; // for later
+    int product = 1; // for later
+    if (temp > 9) // process value
+    {
+        while (temp > 9) // traverse digits
+        {
+            temp /= 10; // remove last digit
+            product *= 10; 
+        }
+    }
+    set->digits[temp] = (Set*)(malloc(sizeof(Set))); // record index corresponding to digit
+    if (set->digits[temp] == NULL) // didn't allocate memeory
+    {
+        fprintf(stderr, "UNABLE TO ALLOCATE MEMEORY FOR SET AT INDEX %d\n", temp);
+        return;
+    }
+    int remainder = value % product;
+    if (product != 1) // there are more digits to save
+    {
+        add(set->digits[temp], remainder);
+    }
+}
+
 int main(void)
 {
     Set* digits = create(); // we've created our digits set
