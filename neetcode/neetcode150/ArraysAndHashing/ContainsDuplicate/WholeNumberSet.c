b8b686bf1cf70964e5bb374298beb1e88e0dc247
Christopher Francisco <cjfg00gl3@gmail.com>
2025-01-26 18:59:19 -0600

    UPDATE WholeNumberSet.c by creating newWholeNumberSet and freeWholeNumberSet function

--- neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/WholeNumberSet.c
+++ neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/WholeNumberSet.c
@@ -4,16 +4,58 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct WholeNumberSet
+#define DIGITS_COUNT (10)
+
+typedef struct WholeNumberSet // Used to store all the digits used
 {
-    struct WholeNumberSet* digits[10]; // 0 through 9
-    bool terminates[10];
+    struct WholeNumberSet* digits[DIGITS_COUNT]; // 0 through 9
+    bool* terminates[DIGITS_COUNT];
 } WholeNumberSet;
 
+WholeNumberSet* newWholeNumberSet() // Used to create a new set
+{
+    WholeNumberSet* set = (WholeNumberSet*)(malloc(sizeof(WholeNumberSet))); // Allocate memory for set
+    if (set == NULL) // set is still NULL
+    {
+        fprintf(stderr, "FAILED TO ALLOCATE MEMORY FOR SET!\n");
+        return NULL;
+    }
+    for (int i = 0; i < DIGITS_COUNT; i++) // Set everything to NULL
+    {
+        set->digits[i] = NULL;
+        set->terminates[i] = NULL;
+    }
+    return set;
+}
+
+void freeWholeNumberSet(WholeNumberSet* set) // Free everything in the set
+{
+    if (set == NULL) // the set is NULL
+    {
+        fprintf(stderr, "FAILED TO FREE MEMEORY OF A NULL SET!\n");
+        return;
+    }
+    for (int i = 0; i < DIGITS_COUNT; i++) // Iterate through each value
+    {
+        if (set->terminates[i] != NULL) // Assure bool* is not null
+        {
+            free(set->terminates[i]);
+        }
+        if (set->digits[i] != NULL) // Assure WholeNumberSet* is not null
+        {
+            freeWholeNumberSet(set->digits[i]);
+        }
+    }
+    free(set);
+}
 
 int main(void)
 {
+    WholeNumberSet* set = newWholeNumberSet();
+    freeWholeNumberSet(set);
+    printf("WORKS!\n");
     return 0;
 }
