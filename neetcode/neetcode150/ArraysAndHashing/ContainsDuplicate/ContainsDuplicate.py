d6c8f9a89cb04cf824ebc82a913c5cba5073d110
Christopher Francisco <cjfg00gl3@gmail.com>
2025-03-31 11:56:06 -0400

    UPDATE ContainsDuplicate.py by updating a comment

--- neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/ContainsDuplicate.py
+++ neetcode/neetcode150/ArraysAndHashing/ContainsDuplicate/ContainsDuplicate.py
@@ -1,4 +1,4 @@
 class Solution:
 
     def contains_duplicate_one(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums)) # yeah
+        return len(nums) != len(set(nums)) # yeah less verbose than c++
