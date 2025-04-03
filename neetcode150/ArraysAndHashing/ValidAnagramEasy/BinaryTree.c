24fc694cf2e8c8d03990409d3f7b8e28a9d52b37
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-03 18:27:05 -0400

    UPDATE BinaryTree.c by creating an insert letter tree node to grow the binary tree of letters

--- neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
+++ neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
@@ -16,7 +16,7 @@ typedef struct LetterTreeNode // Create a node to store the info of a word's let
     Count* count;
 } LetterTreeNode;
 
-LetterTreeNode* createLetterTreeNode(char letter) // Create a letter node given a letter
+LetterTreeNode* createLetterTreeNode(char letter) // Create a letter tree node given a letter
 {
     LetterTreeNode* letterTreeNode = (LetterTreeNode*)malloc(sizeof(LetterTreeNode));
     letterTreeNode->letter = letter;
@@ -31,6 +31,42 @@ void freeLetterTreeNode(LetterTreeNode* letterTreeNode) // Free the memory alloc
 {
     free(letterTreeNode->count);
     free(letterTreeNode);
+}
+
+void insertLetterTreeNode(LetterTreeNode* root, char newLetter) // Insert a letter tree node for a root that will never be null
+{
+    if (root == NULL) // the root is null
+    {
+        fprintf(stderr, "ROOT IS NULL!\n"); // reject it
+        return;
+    }
+    if (newLetter < root->letter) // the character is less than the root
+    {
+        if (root->left == NULL) // go to the left
+        {
+            root->left = createLetterTreeNode(newLetter);
+        }
+        else // keep on looking
+        {
+            insertLetterTreeNode(root->left, newLetter);
+        }
+    }
+    else if (newLetter > root->letter) // the character is greater than the root
+    {
+        if (root->right == NULL) // go to the right
+        {
+            root->right = createLetterTreeNode(newLetter);
+        }
+        else // keep on looking 
+        {
+            insertLetterTreeNode(root->right, newLetter);
+        }
+       
+    }
+    else
+    {
+        root->count->value++; // the character is the same as the root's character / increment count of letter in tree
+    }
 }
 
 int main(void)
