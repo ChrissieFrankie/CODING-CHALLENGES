39d552c5f42988182ef2ef5b05f70ab7c779a220
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-03 17:49:50 -0400

    UPDATE BinaryTree.c create a function createLetterNode to return a LetterNode given a character

--- neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
+++ neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Count // Create a bitfiled to store the frequency of a letter in a word
 {
@@ -11,10 +12,21 @@ typedef struct LetterNode // Create a node to store the info of a word's letter
     Count* count;
 } LetterNode;
 
+LetterNode* createLetterNode(char letter)
+{
+    LetterNode* letterNode = (LetterNode*)malloc(sizeof(LetterNode));
+    letterNode->letter = letter;
+    letterNode->count = (Count*)malloc(sizeof(Count));
+    letterNode->count->value = 1;
+    return letterNode;
+}
+
 int main(void)
 {
     printf("Hello, World!\n");
-
+    LetterNode* a = createLetterNode('a');
+    printf("Letter: %c\n", a->letter);
+    printf("Count: %d\n", a->count->value);
     return 0;
 }
 
