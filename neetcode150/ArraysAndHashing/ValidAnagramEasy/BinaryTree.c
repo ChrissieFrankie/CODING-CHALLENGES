197f2a0b7f130232101af8f2d58d487aee3df747
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-03 17:59:10 -0400

    UPDATE LetterNode typedef struct name to LetterTreeNode

--- neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
+++ neetcode150/ArraysAndHashing/ValidAnagramEasy/BinaryTree.c
@@ -6,31 +6,25 @@ typedef struct Count // Create a bitfiled to store the frequency of a letter in 
     unsigned int value : 4;
 } Count;
 
-typedef struct LetterNode // Create a node to store the info of a word's letter
+typedef struct LetterTreeNode // Create a node to store the info of a word's letter
 {
     unsigned char letter;
     Count* count;
-} LetterNode;
+} LetterTreeNode;
 
-LetterNode* createLetterNode(char letter) // Create a letter node given a letter
+LetterTreeNode* createLetterTreeNode(char letter) // Create a letter node given a letter
 {
-    LetterNode* letterNode = (LetterNode*)malloc(sizeof(LetterNode));
-    letterNode->letter = letter;
-    letterNode->count = (Count*)malloc(sizeof(Count));
-    letterNode->count->value = 1;
-    return letterNode;
-}
-
-void freeLetterNode(LetterNode* letterNode) // Free a letter node to prevent memory leaks
-{
-    free(letterNode->count);
-    free(letterNode);
+    LetterTreeNode* letterTreeNode = (LetterTreeNode*)malloc(sizeof(LetterTreeNode));
+    letterTreeNode->letter = letter;
+    letterTreeNode->count = (Count*)malloc(sizeof(Count));
+    letterTreeNode->count->value = 1;
+    return letterTreeNode;
 }
 
 int main(void)
 {
     printf("Hello, World!\n");
-    LetterNode* a = createLetterNode('a');
+    LetterTreeNode* a = createLetterNode('a');
     printf("Letter: %c\n", a->letter);
     printf("Count: %d\n", a->count->value);
     freeLetterNode(a);
