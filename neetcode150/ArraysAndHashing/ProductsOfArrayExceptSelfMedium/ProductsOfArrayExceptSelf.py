1f12d2f4d95cf70cc86ffcfcce6687c644417287
Christopher Francisco <cjfg00gl3@gmail.com>
2025-04-12 15:46:56 -0400

    CREATE ProductsOfArrayExceptSelf.py brute force solution

---
+++ neetcode150/ArraysAndHashing/ProductsOfArrayExceptSelfMedium/ProductsOfArrayExceptSelf.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def productExceptSelfOne(self, nums: List[int]) -> List[int]: # brute force attempt
+        def product(nums, indices):
+            product = nums[indices[0]] # start result as first num
+            for i in indices[1:]: # iterate through rest of nums
+                product *= nums[i] # use rest of nums as factors
+            return product
+        
+        indices = list(range(len(nums))) # get list of indices
+        res = [] # start result
+        for i in indices: # iterate through indices
+            indices_copy = indices.copy() # copy indices
+            indices_copy.pop(i) # remove self
+            res.append(product(nums, indices_copy)) # add product except self
+        return res # return array products except self
+
+
+                
+        
