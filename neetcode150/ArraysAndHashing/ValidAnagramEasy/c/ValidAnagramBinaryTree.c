4675e3e50802fe32aeb498b2aa4e396b562536ea
Christopher Francisco <cjfg00gl3@gmail.com>
2025-05-08 23:55:08 -0400

    ADD ValidAnagramBinaryTree.c to implement an anagram validation algorithm using a binary tree for letter frequency management, replacing the previous ValidAnagram.c file.

---
+++ neetcode150/ArraysAndHashing/ValidAnagramEasy/c/ValidAnagramBinaryTree.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "BinaryTree.h"
+#include <stdbool.h>
+#include <string.h>
+
+bool validAnagram(char* s, char* t)
+{
+    if (strlen(s) != strlen(t))
+        return false;
+    LetterTreeNode* sBST = stringToLetterBinaryTree(s); // convert s to frequency bst
+    int sLength = countTree(sBST); // count the s frequency bst 
+    while (*t != '\0') // iterate through each char of t
+    {
+        deleteLetterTreeNode(&sBST, *t); // delete t from sBST
+        int newLength = countTree(sBST); // count the s frequency bst after deletion
+        if (newLength == sLength) // if the length remains the same
+        {
+            return false; // something doesn't match
+        }
+        sLength = newLength; // otherwise, the length decreased
+        *t++;
+    }
+    int finalLength = countTree(sBST); // get the final length of s frequency bst
+    return finalLength == 0; // anagram
+}
+
+
+
+int main(void)
+{
+    char* s = "anagram";
+    char* t = "nagaram";
+    bool result = validAnagram(s, t);
+    printf("%s\n", result ? "true" : "false");
+}
