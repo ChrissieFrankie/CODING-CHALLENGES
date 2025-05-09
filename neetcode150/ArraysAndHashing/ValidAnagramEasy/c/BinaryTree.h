04dd02c2462950a83dd34c4afb73f7d22964e947
Christopher Francisco <cjfg00gl3@gmail.com>
2025-05-08 22:18:58 -0400

    ADD BinaryTree.h header file to define structures and functions for letter frequency tree management

---
+++ neetcode150/ArraysAndHashing/ValidAnagramEasy/c/BinaryTree.h
@@ -0,0 +1,27 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+typedef struct Count // Create a bitfield to store the letter frequency
+{
+    unsigned int value : 4
+} Count;
+
+typedef struct LetterTreeNode // Create a node a store the info of a word's letter
+{
+    unsigned char letter;
+    LetterTreeNode *left;
+    LetterTreeNode *right;
+    Count *count;
+} LetterTreeNode;
+
+LetterTreeNode *createLetterTreeNode(char letter);
+void freeLetterTreeNode(LetterTreeNode *letterTreeNode);
+void insertLetterTreeNode(LetterTreeNode **root, char newLetter);
+LetterTreeNode *searchLetterTreeNode(LetterTreeNode *root, char letter);
+Count *searchLetterCount(LetterTreeNode *root, char letter);
+LetterTreeNode *findMinimumLetterTreeNode(LetterTreeNode *root);
+LetterTreeNode *deleteLetterTreeNode(LetterTreeNode **root, char letter);
+void printTreeInOrder(LetterTreeNode *root);
+int countTree(LetterTreeNode *root);
+
+#endif
